cd3e1cbe15dc93dcc3b2cff6e32737eb836b0e74
argianardi <argianardi1435@gmail.com>
2022-11-01 00:57:03 +0700

    install tailwindCSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,8 @@ import "../styles/App.css";
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
