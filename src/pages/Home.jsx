bd683c6ec9446626b4a58be8d0b2ca8aa270d375
argianardi <argianardi1435@gmail.com>
2022-11-01 01:20:41 +0700

    change file extension

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default Home;
