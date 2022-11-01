7db2c016679d7ab8d8cfdbaa3d0113ec141186b9
argianardi <argianardi1435@gmail.com>
2022-11-01 22:11:09 +0700

    add loading view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,52 +12,66 @@ import {
 
 function Home() {
   const [pokemons, setPokemons] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getPokemons();
-  }, []);
-
-  const getPokemons = () => {
+    setLoading(true);
     axios
-      .get("https://pokeapi.co/api/v2/pokemon")
+      .get(currentPageUrl)
       .then((response) => {
-        console.log(response.data.results);
+        console.log(response.data);
+        setLoading(false);
+        setNextPageUrl(response.data.next);
+        setPrevPageUrl(response.data.previous);
         setPokemons(response.data.results);
       })
       .catch((error) => {
         console.log(error.toString());
       });
-  };
+  }, [currentPageUrl]);
 
   return (
-    <div className="overflow-x-hidden">
-      <Header />
-      <div className="w-[90%] sm:w-[80%] md:w-[550px] mx-auto shadow-lg shadow-getblue  my-10 rounded-md">
-        <div className="flex flex-wrap justify-center gap-5 px-5 py-5 mt-4 sm:gap-10 ">
-          {pokemons.map((pokemon) => (
-            <Card
-              key={pokemon.name}
-              pokemonName={pokemon.name}
-              url={pokemon.url}
-            />
-          ))}
+    <>
+      {loading ? (
+        <div className="h-screen min-w-full bh">
+          <p className="mt-[300px] text-5xl text-center">Loading.....</p>
         </div>
-        <div className="flex justify-between p-2 ">
-          <button>
-            <BsFillArrowLeftCircleFill
-              size={45}
-              className="text-getblue hover:text-getorange"
-            />
-          </button>
-          <button>
-            <BsFillArrowRightCircleFill
-              size={45}
-              className="text-getblue hover:text-getorange"
-            />
-          </button>
+      ) : (
+        <div className="overflow-x-hidden">
+          <Header />
+          <div className="w-[90%] sm:w-[80%] md:w-[550px] mx-auto shadow-lg shadow-getblue  my-10 rounded-md">
+            <div className="flex flex-wrap justify-center gap-5 px-5 py-5 mt-4 sm:gap-10 ">
+              {pokemons.map((pokemon) => (
+                <Card
+                  key={pokemon.name}
+                  pokemonName={pokemon.name}
+                  url={pokemon.url}
+                />
+              ))}
+            </div>
+            <div className="flex justify-between p-2 ">
+              <button>
+                <BsFillArrowLeftCircleFill
+                  size={45}
+                  className="text-getblue hover:text-getorange"
+                />
+              </button>
+              <button>
+                <BsFillArrowRightCircleFill
+                  size={45}
+                  className="text-getblue hover:text-getorange"
+                />
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
