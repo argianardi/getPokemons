bf552d11e448ea1de92f6076b33447291794c2ab
argianardi <argianardi1435@gmail.com>
2022-11-02 15:07:08 +0700

    add filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import logo from "../img/PokeBall.ico";
+import { Link } from "react-router-dom";
+
+import { MdSearch } from "react-icons/md";
+import { IoSunny, IoMoon } from "react-icons/io5";
 
 import { Card } from "../components/Card";
 import { Header } from "../components/Header";
@@ -11,6 +16,9 @@ import {
 } from "react-icons/bs";
 
 function Home() {
+  const [pokeData, setPokeData] = useState([]);
+  const [text, setText] = useState("");
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
   const [pokemons, setPokemons] = useState([]);
   const [currentPageUrl, setCurrentPageUrl] = useState(
     "https://pokeapi.co/api/v2/pokemon"
@@ -71,51 +79,73 @@ function Home() {
           <p className="mt-[300px] text-5xl text-center">Loading.....</p>
         </div>
       ) : (
-        <div className="overflow-x-hidden">
+        <>
           <Header />
-          <div className="w-[90%] sm:w-[80%] md:w-[550px] mx-auto shadow-lg shadow-getblue  my-10 rounded-md">
-            <div className="flex flex-wrap justify-center gap-5 px-5 py-5 mt-4 sm:gap-10 ">
-              {pokemons.map((pokemon) => (
-                <Card
-                  pokemons={pokemons}
-                  key={pokemon.id}
-                  pokemonName={pokemon.name}
-                  pokemonId={pokemon.id}
-                  pokemonImg={
-                    pokemon.sprites.front_default
-                      ? pokemon.sprites.front_default
-                      : pokemon.sprites.front_shiny
-                      ? pokemon.sprites.front_shiny
-                      : "https://via.placeholder.com/650x750?text=No+Image"
-                  }
+          <div className="overflow-x-hidden">
+            <div className="w-[85%] sm:w-[80%] lg:w-[800px] px-1 sm:px-4 lg:px-0 lg:-ml-2">
+              <div className="w-full bg-getwhite border sborder-[#D9D9D9] h-[45px] md:h-[50px]  flex justify-between rounded-3xl">
+                <input
+                  type="text"
+                  placeholder="Find the Pokemon..."
+                  onChange={(e) => setText(e.target.value)}
+                  className="w-full px-0 mx-2 text-sm italic outline-none sm:mx-3 bg-limeEboox text-getblue sm:px-2 rounded-3xl bg-getwhite "
                 />
-              ))}
+                <div className="bg-[#D9D9D9] w-14 h-full flex rounded-tr-3xl rounded-br-3xl">
+                  <MdSearch
+                    size={30}
+                    color={"#000000"}
+                    className="self-center mx-auto"
+                  />
+                </div>
+              </div>
             </div>
 
-            <div className="flex justify-between p-2 ">
-              <div className="ml-16 sm:ml-32">
-                {prevPageUrl && (
-                  <button onClick={prevPageUrl ? gotoPrevPage : null}>
-                    <BsFillArrowLeftCircleFill
-                      size={45}
-                      className="text-getblue hover:text-getorange"
+            <div className="w-[90%] sm:w-[80%] md:w-[550px] mx-auto shadow-lg shadow-getblue  my-10 rounded-md">
+              <div className="flex flex-wrap justify-center gap-5 px-5 py-5 mt-4 sm:gap-10 ">
+                {pokemons
+                  .filter((pokemon) => pokemon.name.includes(text))
+                  .map((pokemon) => (
+                    <Card
+                      pokemons={pokemons}
+                      key={pokemon.id}
+                      pokemonName={pokemon.name}
+                      pokemonId={pokemon.id}
+                      pokemonImg={
+                        pokemon.sprites.front_default
+                          ? pokemon.sprites.front_default
+                          : pokemon.sprites.front_shiny
+                          ? pokemon.sprites.front_shiny
+                          : "https://via.placeholder.com/650x750?text=No+Image"
+                      }
                     />
-                  </button>
-                )}
+                  ))}
               </div>
-              <div className="mr-16 sm:mr-32">
-                {nextPageUrl && (
-                  <button onClick={nextPageUrl ? gotoNextPage : null}>
-                    <BsFillArrowRightCircleFill
-                      size={45}
-                      className="text-getblue hover:text-getorange"
-                    />
-                  </button>
-                )}
+
+              <div className="flex justify-between p-2 ">
+                <div className="ml-16 sm:ml-32">
+                  {prevPageUrl && (
+                    <button onClick={prevPageUrl ? gotoPrevPage : null}>
+                      <BsFillArrowLeftCircleFill
+                        size={45}
+                        className="text-getblue hover:text-getorange"
+                      />
+                    </button>
+                  )}
+                </div>
+                <div className="mr-16 sm:mr-32">
+                  {nextPageUrl && (
+                    <button onClick={nextPageUrl ? gotoNextPage : null}>
+                      <BsFillArrowRightCircleFill
+                        size={45}
+                        className="text-getblue hover:text-getorange"
+                      />
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
