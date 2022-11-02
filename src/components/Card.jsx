5a1ee780de089655f519e73178d32845717bd09c
argianardi <argianardi1435@gmail.com>
2022-11-02 12:48:24 +0700

    add link to detail pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const Card = ({ pokemonName, pokemonImg }) => {
+export const Card = ({ pokemonName, pokemonImg, pokemonId }) => {
   return (
-    <div className="w-[200px]  border-4 border-getblue rounded-lg ">
-      <img
-        src={pokemonImg}
-        alt={pokemonImg}
-        className="h-[200px] mx-auto mt-2"
-      />
-      <p className="py-2 mt-5 font-bold text-center capitalize cursor-pointer rounded-b-lglg bg-getblue text-getwhite hover:bg-getorange">
-        {pokemonName}
-      </p>
-    </div>
+    <>
+      <Link to={`/pokemon/${pokemonId}`}>
+        <div className="w-[200px]  border-4 border-getblue rounded-lg ">
+          <img
+            src={pokemonImg}
+            alt={pokemonName}
+            className="h-[200px] mx-auto mt-2"
+          />
+          <p className="py-2 mt-5 font-bold text-center capitalize cursor-pointer rounded-b-lglg bg-getblue text-getwhite hover:bg-getorange">
+            {pokemonName}
+          </p>
+        </div>
+      </Link>
+    </>
   );
 };
